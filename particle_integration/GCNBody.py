7eda4723573a4967ef19b010f685e9cc36ecc8f4
Salvatore <salvatore.ferrone@obspm.fr>
2024-07-22 15:52:09 +0200

    am now integrating  GCNBody

--- particle_integration/GCNBody.py
+++ particle_integration/GCNBody.py
@@ -1,5 +1,6 @@
 import gcs 
 import tstrippy
+import vanilla
 
 def load_GCnames_except_for_the_target(GCname):
     GCnames =list(tstrippy.Parsers.baumgardtMWGCs().data['Cluster'][:])
@@ -7,14 +8,14 @@ def load_GCnames_except_for_the_target(GCname):
     return GCnames
 
 
-def load_arguments(GCnames,GCorbits_potential,montecarlokey):
+def load_perturbers(GCnames,GCorbits_potential,montecarlokey):
     assert isinstance(GCnames,list)
     ts,xs,ys,zs,_,_,_=gcs.extractors.GCOrbits.extract_orbits_from_all_GCS(GCnames,GCorbits_potential,montecarlokey)
     Masses,rh_mes,_,_,_,_,_,_=gcs.extractors.MonteCarloObservables.extract_all_GC_observables(GCnames,montecarlokey)
     r_plums = [gcs.misc.half_mass_to_plummer(rh_m).value for rh_m in rh_mes]
     Masses = [Mass.value for Mass in Masses]
-    args=ts,xs,ys,zs,Masses,r_plums
-    return args
+    perturbers=ts,xs,ys,zs,Masses,r_plums
+    return perturbers
 
 
 def initperturbers(integrator,perturberargs):
---
+++ particle_integration/execute_GCNBody_with_SnapShots.py
@@ -0,0 +1,80 @@
+import gcs
+from gcs import path_handler as ph
+import GCNBody
+import vanilla
+from astropy import units as u
+import datetime
+
+
+if __name__ == "__main__" : 
+    import sys 
+    import os
+    GCname              =   "NGC104"
+    montecarlokey       =   "monte-carlo-000"
+    internal_dynamics   =   "isotropic-plummer"
+    GCorbits_potential  =   "pouliasis2017pii-GCNBody"
+    MWpotential         =   "pouliasis2017pii"
+    NP                  =   int(1e3)
+    T                   =   5e9*u.yr
+    dt                  =   1e5*u.yr
+    NSKIP               =   int(100)
+    
+    attributes = {
+        "NP":NP,
+        "T":T,
+        "dt":dt,
+        "NSKIP":NSKIP,
+        "GCname":GCname,
+        "montecarlokey":montecarlokey,
+        "internal_dynamics":internal_dynamics,
+        "GCorbits_potential":GCorbits_potential,
+        "MWpotential":MWpotential
+    }
+    
+    outfilename=ph.Stream(GCname,NP,GCorbits_potential,internal_dynamics,montecarlokey)
+    snapshotfilename = ph.StreamSnapShots(GCname,NP,GCorbits_potential,internal_dynamics,montecarlokey)
+    cond = False
+    if os.path.exists(snapshotfilename):
+        print(snapshotfilename, "Already exists. \n Skipping!")
+        cond = True
+    if os.path.exists(outfilename):
+        print(outfilename, "Already exists. \n Skipping!")
+        cond = True
+    if cond:
+        sys.exit(0)
+
+    tempdir=ph._TemporaryStreamSnapShots(GCorbits_potential,GCname,NP,internal_dynamics,montecarlokey)
+    ###############################################
+    ########### LOAD THE ARGUMENTS ###############
+    ###############################################
+    staticgalaxy,integrationparameters,initialkinematics,inithostperturber = \
+        vanilla.load_arguments(GCname,montecarlokey,internal_dynamics,GCorbits_potential,MWpotential,NP,T,dt) 
+    GCnames = GCNBody.load_GCnames_except_for_the_target(GCname)
+    perturbers = GCNBody.load_perturbers(GCnames,GCorbits_potential,montecarlokey)
+    ###############################################
+    ########### INITIALIZE THE INTEGRATOR #########
+    ###############################################
+    integrator=vanilla.initialize_vanilla_integrator(staticgalaxy,integrationparameters,initialkinematics,inithostperturber)
+    integrator=vanilla.initialize_write_snapshots(integrator,NSKIP,GCname,tempdir)
+    integrator=GCNBody.initperturbers(integrator,perturbers) 
+    ###############################################
+    ########### PERFORM THE INTEGRATION ###########
+    ###############################################
+    starttime=datetime.datetime.now()
+    phase_space,tesc = vanilla.leapfrogtofinalpositions(integrator)
+    endtime=datetime.datetime.now()
+    print("Integration took",endtime-starttime)
+    ################################################
+    ############ THE SAVIOR OF THE DATA ############
+    ################################################
+    attributes["GCnames"]=GCnames
+    gcs.writers.Stream.stream(outfilename,phase_space,tesc,attributes)
+    print(outfilename, "saved")
+    ################################################
+    ############## SAVE THE SNAP SHOTS #############
+    ################################################
+    T,dt,Nstep,tsampling=gcs.misc.get_time_sampling_from_years_to_integration_units(T=T,dt=dt)
+    snapshottimesampling = tsampling[::NSKIP]
+    gcs.writers.Stream.StreamSnapShots(snapshotfilename,snapshottimesampling,attributes,tempdir)
+    print(snapshotfilename, "saved")
+    

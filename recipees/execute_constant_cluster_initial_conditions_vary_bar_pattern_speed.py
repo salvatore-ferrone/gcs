c4cc66f2926d554dd5aa4ab942834482c7e0668d
salvatore-ferrone <salvatore.ferrone.1996@gmail.com>
2025-01-03 18:27:26 +0000

    added recipee to use the same exact initial conditions

---
+++ recipees/execute_constant_cluster_initial_conditions_vary_bar_pattern_speed.py
@@ -0,0 +1,98 @@
+"""
+Will call the function to execute the constant cluster initial conditions
+from stream_evolution_in_barred_potential.py
+
+"""
+
+
+
+
+import stream_evolution_in_barred_potential as sebp
+from astropy import units as u
+import numpy as np 
+import tstrippy
+import sys 
+import multiprocessing as mp 
+
+omega_min, omega_max = 30,66
+omega_step = 0.25
+bar_pattern_speeds = np.arange(omega_min,omega_max+omega_step,omega_step)
+bar_pattern_speeds_m_kpc_s=np.array(1000*bar_pattern_speeds, dtype=int)
+
+
+DOMULTIPROCESSING = True
+def main(monte_carlo_index):
+
+    # do this a handful of times 
+    GCname              =   "Pal5"
+    MWpotential         =   "pouliasis2017pii"
+    internal_dynamics   =   "isotropic-plummer"
+    barname             =   "longmuralibar"
+    barparams           =   [22968000000, 4, 1, 0.5]
+    barpoly_ferrone_2023=   [0.4363323129985824, 38]
+    NP                  =   int(1e2)
+    integrationtime     =   5e9*u.yr
+    dt                  =   1e5*u.yr
+    NSKIP               =   int(10)  
+    description         =   "Integrating star-particles with in a globular cluster in a galaxy with a bar. The cluster initial conditions were passed as an argument, and the particle initial conditions were passed as an argument. Therefore, these results can be compared to others where the bar properties are different."
+    writestream         =   False  
+
+    GCdata = tstrippy.Parsers.baumgardtMWGCs()
+    ###### GET THE INITIAL CONDITIONS OF THE CLUSTER IN PHASE SPACE ######
+    means,cov=GCdata.getGCCovarianceMatrix(GCname)
+    if monte_carlo_index==0:
+        RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m = means
+    else:   
+        temp=np.random.multivariate_normal(means,cov,1)
+        RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=temp[0]
+    
+    cluster_initial_conditions=RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
+
+    ###### MAKE THE PLUMMER SPHERE ######
+    G=tstrippy.Parsers.potential_parameters.G
+    xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,Mass,rh_m,NP)
+    particle_initial_conditions = np.array([xp,yp,zp,vxp,vyp,vzp])
+
+
+    # iterate over the bar pattern speeds
+
+
+    if DOMULTIPROCESSING:
+        ncpu = mp.cpu_count()
+        Pool=mp.Pool(ncpu)
+
+        for index in range(len(bar_pattern_speeds)):
+            barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
+            temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
+            print(barpoly)
+            Pool.apply_async(sebp.constant_cluster_initial_conditions, args=(cluster_initial_conditions,particle_initial_conditions,GCname,MWpotential,internal_dynamics,barname,barparams,barpoly,integrationtime,dt,NSKIP,temp_base_name,description,writestream))
+        Pool.close()
+        Pool.join()
+        Pool.terminate()
+    else:
+        for index in range(len(bar_pattern_speeds)):
+
+            barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
+            temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
+
+            sebp.constant_cluster_initial_conditions(
+                cluster_initial_conditions=cluster_initial_conditions,
+                particle_initial_conditions=particle_initial_conditions,
+                GCname=GCname,
+                MWpotential=MWpotential,
+                internal_dynamics=internal_dynamics,
+                barname=barname,
+                barparams=barparams,
+                barpoly=barpoly,
+                integrationtime=integrationtime,
+                dt=dt,
+                NSKIP=NSKIP,
+                temp_base_name=temp_base_name,
+                description=description,
+                writestream=writestream)
+
+    print("Done with monte carlo index {:d}".format(monte_carlo_index))
+    return None
+if __name__=="__main__":
+    montecarloindex=sys.argv[1]
+    main(int(montecarloindex))
--- recipees/stream_evolution_in_barred_potential.py
+++ recipees/stream_evolution_in_barred_potential.py
@@ -23,6 +23,176 @@ author_affiliation = "Sapienza University of Rome"
 author_email = "salvatore.ferrone@uniroma1.it"
 description = "Integrating star-particles with in a globular cluster"
 
+def constant_cluster_initial_conditions(
+        cluster_initial_conditions,
+        particle_initial_conditions,
+        GCname              =   "Pal5",
+        MWpotential         =   "pouliasis2017pii",
+        internal_dynamics   =   "isotropic-plummer",
+        barname             =   "longmuralibar",
+        barparams           =   [22968000000, 4, 1, 0.5],
+        barpoly             =   [0.4363323129985824, 38],
+        integrationtime     =   5e9*u.yr,
+        dt                  =   1e5*u.yr,
+        NSKIP               =   int(10),
+        temp_base_name      =   "constant_cluster_initial_conditions",
+        description         =   "Integrating star-particles with in a globular cluster in a galaxy with a bar. The cluster initial conditions were passed as an argument, and the particle initial conditions were passed as an argument. Therefore, these results can be compared to others where the bar properties are different.",
+        writestream         =   True
+          
+):
+    
+    NP = len(particle_initial_conditions[0]) # rederive since it is an input argument 
+    # CURRENT OUTPUT FILE
+    outdir = ph.paths['simulations'] + "StreamEvolutionInBarredPotential/" + MWpotential + "/" + barname + "/" + GCname + "/" + str(NP) + "/"
+    outname = GCname+"_"+temp_base_name+"-stream.hdf5"
+    os.makedirs(outdir,exist_ok=True)
+    G=tstrippy.Parsers.potential_parameters.G
+    barparams_with_G = [G]+barparams
+    ################################################
+    #### 1. ORBIT of the HOST GLOBULAR CLUSTER #####
+    ################################################
+    # LOAD THE STATIC GALAXY
+    staticgalaxy_params = tstrippy.Parsers.potential_parameters.pouliasis2017pii()
+    staticgalaxy = (MWpotential,staticgalaxy_params)
+    # adjust for the bar 
+    static_galaxy_adjusted = functions.adjust_disc_mass(staticgalaxy,barparams)    
+    # set the integration time.
+    unitT = u.s*(u.kpc/u.km)
+    NSTEP = int(integrationtime/dt)
+    dt=dt.to(unitT).value
+    T0=0
+    integrationparameters = (T0,dt,NSTEP)    
+    # load the position of the host cluster
+    RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=cluster_initial_conditions
+    x,y,z,vx,vy,vz = functions.sky_to_galactocentric(RA,DEC,Rsun,RV,mualpha,mu_delta)
+    initialkinematics = (x,y,z,vx,vy,vz) # the velocities are positive because setbackwardorbit() makes them negative intenral to tstrippy
+
+    # now integrate the host backward 
+    integrator = tstrippy.integrator
+    integrator.setstaticgalaxy(*static_galaxy_adjusted)
+    integrator.setintegrationparameters(*integrationparameters)
+    integrator.setinitialkinematics(*initialkinematics)
+    integrator.initgalacticbar(barname,barparams_with_G,barpoly)
+    integrator.setbackwardorbit()
+    xt,yt,zt,vxt,vyt,vzt = integrator.leapfrogintime(NSTEP,1)
+    timesteps = integrator.timestamps.copy()
+    # flip and change the sign of the velocities
+    xt=xt[0,::-1]
+    yt=yt[0,::-1]
+    zt=zt[0,::-1]
+    vxt=-vxt[0,::-1]
+    vyt=-vyt[0,::-1]
+    vzt=-vzt[0,::-1]
+    timesteps=timesteps[::-1]
+    integrator.deallocate()
+
+
+
+    ################################################
+    #### 2. ORBIT OF THE STREAM PARTICLES ##########
+    ################################################
+
+    # make a plummer sphere
+    xp,yp,zp,vxp,vyp,vzp = particle_initial_conditions
+    # get plummer radius
+    rplummer = gcs.misc.half_mass_to_plummer(rh_m)
+    # move about the host
+    xp,yp,zp=xp+xt[0],yp+yt[0],zp+zt[0]
+    vxp,vyp,vzp=vxp+vxt[0],vyp+vyt[0],vzp+vzt[0]
+
+    # adjust the integration time to set the initial time to the last time of the host
+    integrationparameters = (timesteps[0],dt,NSTEP)
+    
+    # perpare the integrator
+    integrator = tstrippy.integrator
+    integrator.setstaticgalaxy(*static_galaxy_adjusted)
+    integrator.setintegrationparameters(*integrationparameters)
+    integrator.setinitialkinematics(xp,yp,zp,vxp,vyp,vzp)
+    integrator.initgalacticbar(barname,barparams_with_G,barpoly)
+    integrator.inithostperturber(timesteps,xt,yt,zt,vxt,vyt,vzt,Mass,rplummer)
+    # set the writestream
+    temporary_dir_binary_files = "No temporary files were used"
+    if writestream:
+        temporary_dir_binary_files = ph.paths['temporary'] + MWpotential + '/'+ barname + '/'+ GCname + '/' + temp_base_name + '/'
+        os.makedirs(temporary_dir_binary_files,exist_ok=True)
+        print("Writing binary files to", temporary_dir_binary_files)
+        integrator.initwritestream(NSKIP,GCname,temporary_dir_binary_files)
+    # perform the integration
+    integrator.leapfrogtofinalpositions()
+    # copy the final positions
+    xf,yf,zf = integrator.xf.copy(),integrator.yf.copy(),integrator.zf.copy()
+    vxf,vyf,vzf=integrator.vxf.copy(),integrator.vyf.copy(),integrator.vzf.copy()
+    tesc=integrator.tesc.copy()
+    stream_final = np.array([xf,yf,zf,vxf,vyf,vzf])
+    integrator.deallocate()
+
+    ################################################
+    #### 3. SAVE THE DATA ##########################
+    ################################################
+    # store the host orbit
+    hostorbit = np.array([xt,yt,zt,vxt,vyt,vzt])
+    # set the attributes of the file
+    initialconditions = [RA,DEC,Rsun,RV,mualpha,mu_delta]
+    attributes={
+        "GCname":GCname,
+        "MWpotential": MWpotential,
+        "staticgalaxy": static_galaxy_adjusted[1],
+        "barname": barname,
+        "barparams": barparams,
+        "barpoly": barpoly,
+        "internal-dynamics": internal_dynamics,
+        "NP": NP,
+        "integrationtime": integrationtime,
+        "dt": dt,
+        "NSKIP": NSKIP,
+        "temp_base_name": temp_base_name,
+        "temporary_dir_binary_files": temporary_dir_binary_files,
+        "initialconditions": initialconditions
+    }
+    attributes["author"] = author
+    attributes["author_affiliation"] = author_affiliation
+    attributes["author_email"] = author_email
+    attributes["description"] = description
+    attributes["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+
+    # load in the snap shots 
+    
+
+
+    with h5py.File(outdir+outname, 'w') as outfile:
+        outfile.create_group("HostOrbit")
+        outfile['HostOrbit'].create_dataset("timestamps",data=timesteps)
+        outfile['HostOrbit'].create_dataset("orbit",data=hostorbit)
+        outfile['HostOrbit'].create_dataset("Mass",data=Mass)
+        outfile['HostOrbit'].create_dataset("rh_m",data=rh_m)
+
+        # save the stream snapshots
+        outfile.create_group("Stream")
+        if writestream:
+            snapshottimesampling = timesteps[::NSKIP]
+        else:
+            snapshottimesampling = np.array([timesteps[-1]])
+        NSTAMPS = len(snapshottimesampling)
+        outfile['Stream'].create_dataset("timestamps",data=snapshottimesampling)
+        outfile["Stream"].create_dataset("NSTAMPS",data=NSTAMPS)
+        outfile["Stream"].create_dataset("tesc",data=tesc)
+        outfile["Stream"].create_group("StreamSnapShots")
+        if writestream:
+            filenames,indexes=get_temp_snapshot_filenames(temporary_dir_binary_files)
+            for i in range(len(filenames)):
+                phase_space = read_fortran_stream_binary_file(temporary_dir_binary_files+filenames[i])
+                outfile["Stream"]["StreamSnapShots"].create_dataset(str(indexes[i]),data=phase_space)
+
+        outfile["Stream"]["StreamSnapShots"].create_dataset("final",data=stream_final)
+
+
+        for attr in attributes:
+            outfile.attrs[attr] = attributes[attr]
+    print(outdir+outname, "saved")
+
+
+
+
 def main(
     GCname              =   "Pal5",
     MWpotential         =   "pouliasis2017pii",

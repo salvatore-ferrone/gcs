31c0243a864ca89216dc79aa2d00c89cf52f6abd
sferrone <salvatore.ferrone@obspm.fr>
2025-01-06 15:51:19 +0100

    updated to new format

--- gcs/extractors/MonteCarloObservables.py
+++ gcs/extractors/MonteCarloObservables.py
@@ -3,17 +3,12 @@ import gcs.path_handler as ph
 from astropy import units as u
 
 
-def extract_GC_observables(filename,montecarlokey,):
+def extract_GC_observables(filename,montecarloindex):
+    assert (isinstance(filename,str)), "filename must be a string"
+    assert (isinstance(montecarloindex,int)), "montecarloindex must be an integer"
     with h5py.File(filename,'r') as filehdf5:
-        RA          =   filehdf5[montecarlokey]['RA'][0]        *u.Unit(filehdf5.attrs['RA'])
-        DEC         =   filehdf5[montecarlokey]['DEC'][0]       *u.Unit(filehdf5.attrs['DEC'])
-        rh_m        =   filehdf5[montecarlokey]['rh_m'][0]      *u.Unit(filehdf5.attrs['rh_m'])
-        Mass        =   filehdf5[montecarlokey]['Mass'][0]      *u.Unit(filehdf5.attrs['Mass'])
-        Rsun        =   filehdf5[montecarlokey]['Rsun'][0]      *u.Unit(filehdf5.attrs['Rsun'])
-        RV          =   filehdf5[montecarlokey]['RV'][0]        *u.Unit(filehdf5.attrs['RV'])
-        mualpha     =   filehdf5[montecarlokey]['mualpha'][0]   *u.Unit(filehdf5.attrs['mualpha'])
-        mu_delta    =   filehdf5[montecarlokey]['mu_delta'][0]  *u.Unit(filehdf5.attrs['mu_delta'])
-    return  Mass,rh_m,RA,DEC,Rsun,RV,mualpha,mu_delta
+        RA,DEC,rh_m,Mass,Rsun,RV,mualpha,mu_delta=filehdf5['initial_conditions'][montecarloindex]
+    return  RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
 
 def extract_all_GC_observables(GCnames,montecarlokey):
     assert(isinstance(GCnames,list))
@@ -23,7 +18,7 @@ def extract_all_GC_observables(GCnames,montecarlokey):
         filename=ph.MonteCarloObservables(GCnames[i])
         
 
-        Mass,rh_m,RA,DEC,Rsun,RV,mualpha,mu_delta=extract_GC_observables(filename,montecarlokey)
+        RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=extract_GC_observables(filename,montecarlokey)
         Masses.append(Mass)
         rh_mes.append(rh_m)
         RAes.append(RA)
@@ -34,4 +29,4 @@ def extract_all_GC_observables(GCnames,montecarlokey):
         mu_deltaes.append(mu_delta)
         
 
-    return Masses,rh_mes,RAes,DECes,Rsunes,RVes,mualphaes,mu_deltaes
+    return RAes,DECes,Rsunes,RVes,mualphaes,mu_deltaes,Masses,rh_mes

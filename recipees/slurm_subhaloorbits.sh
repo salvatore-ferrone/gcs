f46092a3f186f4d759e1b819b8753a07e3cfd33b
sferrone <salvatore.ferrone@obspm.fr>
2025-02-04 19:00:27 +0100

    conda stuff

--- recipees/slurm_subhaloorbits.sh
+++ recipees/slurm_subhaloorbits.sh
@@ -2,17 +2,16 @@
 #SBATCH --output=DM_subhalo_orbits.out
 #SBATCH --error=DM_subhalo_orbits.err
 #SBATCH --job-name=NGC4590_5K_particles
-#SBATCH --partition=short
-#SBATCH --time=30
+#SBATCH --partition=medium
+#SBATCH --time=1359
 
 
 # Load modules
 module purge
 module load python
-# source /obs/sferrone/py-env-gc/bin/activate
 
 # Activate your conda environment
-source /obs/sferrone/.bashrc
+source /data/sferrone/miniconda3/etc/profile.d/conda.sh
 conda activate gcs
 
 

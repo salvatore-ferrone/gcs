f9a6295e97c76f6503efbb5b84c6e1939fd35c11
sferrone <salvatore.ferrone@obspm.fr>
2025-02-10 18:41:48 +0100

    fixed assert statement

--- gcs/extractors/MonteCarloObservables.py
+++ gcs/extractors/MonteCarloObservables.py
@@ -4,15 +4,15 @@ from astropy import units as u
 
 
 def extract_GC_observables(filename,montecarloindex):
-    assert (isinstance(filename,str)), "filename must be a string but instead was: "+str(type(filename))
-    assert (isinstance(montecarloindex,int)), "montecarloindex must be an integer but instead was: "+str(type(montecarloindex))
+    assert isinstance(filename,str), "filename must be a string but instead was: "+str(type(filename))
+    assert isinstance(montecarloindex,int), "montecarloindex must be an integer but instead was: "+str(type(montecarloindex))
     with h5py.File(filename,'r') as filehdf5:
         RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=filehdf5['initial_conditions'][montecarloindex]
     return  RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
 
 def extract_all_GC_observables(GCnames,montecarloindex):
-    assert(isinstance(GCnames,list), "GCnames must be a list but instead was: "+str(type(GCnames)))
-    assert(isinstance(montecarloindex,int), "montecarlokey must be a string but instead was: "+str(type(montecarloindex)))
+    assert isinstance(GCnames,list), "GCnames must be a list but instead was: "+str(type(GCnames))
+    assert isinstance(montecarloindex,int), "montecarlokey must be a string but instead was: "+str(type(montecarloindex))
     Masses,rh_mes,RAes,DECes,Rsunes,RVes,mualphaes,mu_deltaes=[],[],[],[],[],[],[],[]
     for i in range(len(GCnames)):
         filename=ph.MonteCarloObservables(GCnames[i])

e6cb1488a16cb4c36bebcf17403bc46e293ad1ab
sferrone <salvatore.ferrone@obspm.fr>
2025-02-11 12:28:55 +0100

    executed with 10k particles

--- recipees/slurm_execute_GCNBody_Palomar5_Mass_Increase.sh
+++ recipees/slurm_execute_GCNBody_Palomar5_Mass_Increase.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --output=pal5_increase_mass.out
-#SBATCH --error=pal5_increase_mass.err
-#SBATCH --job-name=pal5_increase_mass
-#SBATCH --partition=short
-#SBATCH --time=30
-#SBATCH --array=1-10
+#SBATCH --output=10000P.out
+#SBATCH --error=10000P.err
+#SBATCH --job-name=10000P
+#SBATCH --partition=medium
+#SBATCH --time=1339
+#SBATCH --array=[0,2,5,9,19,27,35]
 
 # Load modules
 module purge

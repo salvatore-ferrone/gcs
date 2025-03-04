2b0dcdaefd733d519c73e731191aa5ab8a7cc0e4
sferrone <salvatore.ferrone@obspm.fr>
2025-03-04 16:15:07 +0100

    for making the call independent

---
+++ recipees/mass_size_grid_params.py
@@ -0,0 +1,28 @@
+# making the input parameters for plummer_pal5_mass_radius_grid.py so that I can launch the jobs in parallel
+# I am looping over the particle number, mass, and radius 
+import numpy as np 
+
+
+
+# MASS GRID AND RADIUS GRID
+SIZE_GRID = 5
+N_MASS_SAMPLING = SIZE_GRID
+N_RADIUS_SAMPLING = SIZE_GRID # square grid
+MASS_GRID = np.logspace(4,5.2,N_MASS_SAMPLING) # in Msun
+RADIUS_GRID = np.linspace(5,30,N_RADIUS_SAMPLING)/1000 # in kpc
+
+
+# WE ARE ALSO ITERATING OVER THE PARTICLES
+start = 9000
+stop = 9900
+step = 100
+mysequence = np.arange(start,stop+step,step,dtype=int)
+mysequence[0]=mysequence[0]//2
+
+# OUTPUT FILE
+output_file = "mass_radius_grid_params.txt"
+for mass in MASS_GRID:
+    for radius in RADIUS_GRID:
+        for n_particles in mysequence:
+            with open(output_file,"a") as f:
+                f.write(f"{mass} {radius} {n_particles}\n")

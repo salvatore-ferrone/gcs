bff13aa8f2bb26f74ff49026ad0a8eb941fe8176
sferrone <salvatore.ferrone@obspm.fr>
2025-03-06 13:29:48 +0100

    got rid of useless module and conda calls

--- organized_recipees/NGC6397_WITH_BAR/slurm_execute_constant_cluster_initial_conditions_vary_bar_pattern_speed.sh
+++ organized_recipees/NGC6397_WITH_BAR/slurm_execute_constant_cluster_initial_conditions_vary_bar_pattern_speed.sh
@@ -7,21 +7,14 @@
 #SBATCH --array=[1-660]
 
 
-# Load modules
-module purge
-module load python
-# source /obs/sferrone/py-env-gc/bin/activate
+## make sure the conda environment is activated before running this script
 
-# Activate your conda environment
-source /obs/sferrone/.bashrc
-conda activate gcs
 
-paramfile="NGC6397_WITH_BAR/constant_cluster_initial_conditions_vary_bar_pattern_speed_params.txt"
+paramfile="arguments_for_constant_cluster_initial_conditions_vary_bar_pattern_speed.txt"
 paramline=$(sed -n "$((SLURM_ARRAY_TASK_ID))p" $paramfile)
 montecarloindex=$(echo $paramline | awk '{print $1}')
 barpatternspeedindex=$(echo $paramline | awk '{print $2}')
 numberofparticles=$(echo $paramline | awk '{print $3}')
-
 
 
 # Run script

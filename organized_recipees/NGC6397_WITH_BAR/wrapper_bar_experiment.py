6b18b8ae84a1e540f31d9a15fc35308740894655
sferrone <salvatore.ferrone@obspm.fr>
2025-03-06 14:04:02 +0100

    adding the full experiment parameters to the script

--- organized_recipees/NGC6397_WITH_BAR/wrapper_bar_experiment.py
+++ organized_recipees/NGC6397_WITH_BAR/wrapper_bar_experiment.py
@@ -13,19 +13,29 @@ import gcs
 import multiprocessing as mp 
 import datetime
 
+#########################################################
+##### THE RANGE OF PARAMETERS FOR THE WE WILL PROBE #####
+#########################################################
+
+# the bar pattern speeds
 omega_min, omega_max = 25,66
 omega_step = 0.25
 bar_pattern_speeds = np.arange(omega_min,omega_max+omega_step,omega_step)
 bar_pattern_speeds_m_kpc_s=np.array(1000*bar_pattern_speeds, dtype=int)
+
+# the bar length 
+
+
+# the bar mass
+
+
+# the bar angle
 
 # The parameters of of the code 
 GCname              =   "NGC6397"
 MWpotential         =   "pouliasis2017pii"
 internal_dynamics   =   "isotropic-plummer"
 barname             =   "longmuralibar"
-barparams           =   [22968000000, 4, 1, 0.5]
-barpoly_ferrone_2023=   [0.4363323129985824, 38]
-# NP                  =   int(1e5)
 integrationtime     =   5e9*u.yr
 dt                  =   1e5*u.yr
 NSKIP               =   int(10)  
@@ -33,7 +43,7 @@ description         =   "Integrating star-particles with in a globular cluster i
 writestream         =   False  
 DOMULTIPROCESSING   =   False
 
-def single_pattern_speed(monte_carlo_index,bar_pattern_speed_index, NP):
+def wrapper(bar_pattern_speed_index, bar_angle_index, bar_mass_index, bar_length_index, montecarloindex, NP):
     """
         Intended through parallelization with a slrum and the slurm job array
     """
@@ -90,96 +100,99 @@ def single_pattern_speed(monte_carlo_index,bar_pattern_speed_index, NP):
 
 
 
-### THESE TWO FUNCTIONS WERE USED FOR DEVELOPMENT ON HAMPI
-def multiprocessingloop(monte_carlo_index):
-    """
-        This is for multiprocessing, intended mainly for parallelization on nhampi. 
+# ### THESE TWO FUNCTIONS WERE USED FOR DEVELOPMENT ON HAMPI
+# def multiprocessingloop(monte_carlo_index):
+#     """
+#         This is for multiprocessing, intended mainly for parallelization on nhampi. 
 
-    """
+#     """
 
-    assert isinstance(monte_carlo_index, int), "monte_carlo_index must be an integer"
-    ###### GET THE INITIAL CONDITIONS OF THE CLUSTER IN PHASE SPACE ######
-    # get the name of the master file of initial conditions
-    initial_conditions_file_name = gcs.path_handler.MonteCarloObservables(GCname)
-    RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=gcs.extractors.MonteCarloObservables.extract_GC_observables(initial_conditions_file_name,monte_carlo_index)
-    cluster_initial_conditions=RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
+#     assert isinstance(monte_carlo_index, int), "monte_carlo_index must be an integer"
+#     ###### GET THE INITIAL CONDITIONS OF THE CLUSTER IN PHASE SPACE ######
+#     # get the name of the master file of initial conditions
+#     initial_conditions_file_name = gcs.path_handler.MonteCarloObservables(GCname)
+#     RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=gcs.extractors.MonteCarloObservables.extract_GC_observables(initial_conditions_file_name,monte_carlo_index)
+#     cluster_initial_conditions=RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
 
-    ###### MAKE THE PLUMMER SPHERE ######
-    G=tstrippy.Parsers.potential_parameters.G
-    xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,Mass,rh_m,NP)
-    particle_initial_conditions = np.array([xp,yp,zp,vxp,vyp,vzp])
-
-
-    # iterate over the bar pattern speeds
-    starttime = datetime.datetime.now()
-
-    ncpu = mp.cpu_count()
-    Pool=mp.Pool(ncpu)
-    for index in range(len(bar_pattern_speeds)):
-        barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
-        temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
-        Pool.apply_async(sebp.main, args=(cluster_initial_conditions,particle_initial_conditions,GCname,MWpotential,internal_dynamics,barname,barparams,barpoly,integrationtime,dt,NSKIP,temp_base_name,description,writestream))
-    Pool.close()
-    Pool.join()
-    Pool.terminate()
-
-    endtime = datetime.datetime.now()
-    print("Done with monte carlo index {:d}".format(monte_carlo_index))
-    print("Elapsed time: ", endtime-starttime)
-    return None
+#     ###### MAKE THE PLUMMER SPHERE ######
+#     G=tstrippy.Parsers.potential_parameters.G
+#     xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,Mass,rh_m,NP)
+#     particle_initial_conditions = np.array([xp,yp,zp,vxp,vyp,vzp])
 
 
-def simpleloop(monte_carlo_index):
-    """
-        If you want to run this in a simple loop, without multiprocessing
-        this is looping over the different pattern speeds 
+#     # iterate over the bar pattern speeds
+#     starttime = datetime.datetime.now()
 
-    """
+#     ncpu = mp.cpu_count()
+#     Pool=mp.Pool(ncpu)
+#     for index in range(len(bar_pattern_speeds)):
+#         barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
+#         temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
+#         Pool.apply_async(sebp.main, args=(cluster_initial_conditions,particle_initial_conditions,GCname,MWpotential,internal_dynamics,barname,barparams,barpoly,integrationtime,dt,NSKIP,temp_base_name,description,writestream))
+#     Pool.close()
+#     Pool.join()
+#     Pool.terminate()
 
-    assert isinstance(monte_carlo_index, int), "monte_carlo_index must be an integer"
-    ###### GET THE INITIAL CONDITIONS OF THE CLUSTER IN PHASE SPACE ######
-    # get the name of the master file of initial conditions
-    initial_conditions_file_name = gcs.path_handler.MonteCarloObservables(GCname)
-    RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=gcs.extractors.MonteCarloObservables.extract_GC_observables(initial_conditions_file_name,monte_carlo_index)
-    cluster_initial_conditions=RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
-
-    ###### MAKE THE PLUMMER SPHERE ######
-    G=tstrippy.Parsers.potential_parameters.G
-    xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,Mass,rh_m,NP)
-    particle_initial_conditions = np.array([xp,yp,zp,vxp,vyp,vzp])
+#     endtime = datetime.datetime.now()
+#     print("Done with monte carlo index {:d}".format(monte_carlo_index))
+#     print("Elapsed time: ", endtime-starttime)
+#     return None
 
 
-    # iterate over the bar pattern speeds
-    starttime = datetime.datetime.now()
+# def simpleloop(monte_carlo_index):
+#     """
+#         If you want to run this in a simple loop, without multiprocessing
+#         this is looping over the different pattern speeds 
 
-    for index in range(len(bar_pattern_speeds)):
-        barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
-        temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
-        sebp.main(
-            cluster_initial_conditions=cluster_initial_conditions,
-            particle_initial_conditions=particle_initial_conditions,
-            GCname=GCname,
-            MWpotential=MWpotential,
-            internal_dynamics=internal_dynamics,
-            barname=barname,
-            barparams=barparams,
-            barpoly=barpoly,
-            integrationtime=integrationtime,
-            dt=dt,
-            NSKIP=NSKIP,
-            temp_base_name=temp_base_name,
-            description=description,
-            writestream=writestream)
+#     """
 
-    endtime = datetime.datetime.now()
-    print("Done with monte carlo index {:d}".format(monte_carlo_index))
-    print("Elapsed time: ", endtime-starttime)
-    return None
+#     assert isinstance(monte_carlo_index, int), "monte_carlo_index must be an integer"
+#     ###### GET THE INITIAL CONDITIONS OF THE CLUSTER IN PHASE SPACE ######
+#     # get the name of the master file of initial conditions
+#     initial_conditions_file_name = gcs.path_handler.MonteCarloObservables(GCname)
+#     RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m=gcs.extractors.MonteCarloObservables.extract_GC_observables(initial_conditions_file_name,monte_carlo_index)
+#     cluster_initial_conditions=RA,DEC,Rsun,RV,mualpha,mu_delta,Mass,rh_m
+
+#     ###### MAKE THE PLUMMER SPHERE ######
+#     G=tstrippy.Parsers.potential_parameters.G
+#     xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,Mass,rh_m,NP)
+#     particle_initial_conditions = np.array([xp,yp,zp,vxp,vyp,vzp])
+
+
+#     # iterate over the bar pattern speeds
+#     starttime = datetime.datetime.now()
+
+#     for index in range(len(bar_pattern_speeds)):
+#         barpoly = [barpoly_ferrone_2023[0], bar_pattern_speeds[index]]
+#         temp_base_name = "constant_cluster_initial_conditions_{:d}_bar_pattern_speed_{:d}_m_kpc_s".format(monte_carlo_index,bar_pattern_speeds_m_kpc_s[index])
+#         sebp.main(
+#             cluster_initial_conditions=cluster_initial_conditions,
+#             particle_initial_conditions=particle_initial_conditions,
+#             GCname=GCname,
+#             MWpotential=MWpotential,
+#             internal_dynamics=internal_dynamics,
+#             barname=barname,
+#             barparams=barparams,
+#             barpoly=barpoly,
+#             integrationtime=integrationtime,
+#             dt=dt,
+#             NSKIP=NSKIP,
+#             temp_base_name=temp_base_name,
+#             description=description,
+#             writestream=writestream)
+
+#     endtime = datetime.datetime.now()
+#     print("Done with monte carlo index {:d}".format(monte_carlo_index))
+#     print("Elapsed time: ", endtime-starttime)
+#     return None
 
 
 
 if __name__=="__main__":
     print(len(sys.argv), sys.argv, "arguments given")
+
+    barparams           =   [22968000000, 4, 1, 1]
+    barpoly_ferrone_2023=   [0.4363323129985824, 38]    
     if len (sys.argv)==4:
         scriptname = sys.argv[0]
         montecarloindex=int(sys.argv[1])

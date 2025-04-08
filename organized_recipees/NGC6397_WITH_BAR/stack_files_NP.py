bad25347fd959590d3cfd40cccbc399bcb4bfd47
sferrone <salvatore.ferrone@obspm.fr>
2025-04-08 10:36:15 +0200

    started script to stack the files in particle number

---
+++ organized_recipees/NGC6397_WITH_BAR/stack_files_NP.py
@@ -0,0 +1,6 @@
+"""
+Since I can run simulations by chopping the data into smaller pieces, I can run them in parallel.
+This script will take the output of the simulation and stack them together.
+
+ONLY chopped by particle number 
+"""

9720f57d0cdfc60c1e95634ab415b2953a4a64c8
sferrone <salvatore.ferrone@obspm.fr>
2025-04-08 18:55:23 +0200

    made script to iterate over particle number

---
+++ organized_recipees/VaryPerturberParams/arguments.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import varyPerturberParams
+
+GAURD = 999 # cannot run more than 999 simulations at once 
+
+# number of simulations for the internal grid
+N_internal=varyPerturberParams.SIZE_GRID*varyPerturberParams.SIZE_GRID
+
+# number of simulations for the perturber grid
+mass_radius=np.loadtxt("sub_halo_mass_radius.txt",dtype=float)
+N_perturbers = mass_radius.shape[0]
+
+
+# WE ARE ALSO ITERATING OVER THE PARTICLES
+# compute the number of particle batches 
+DIVISOR = GAURD//(N_perturbers*N_internal) 
+TOTAL = 1e5
+STEP=1
+NPs = np.arange(TOTAL//DIVISOR - DIVISOR//2  ,
+                        TOTAL//DIVISOR + DIVISOR//2 ,
+                        STEP,dtype=int)
+
+N_particle_batches = len(NPs)
+
+outputfile="arguments.txt"
+
+with open(outputfile,"w") as f:
+    f.write("# n_particles internal_index perturber_index\n")
+
+
+N_batches = len(NPs)*N_internal*N_perturbers
+for i in range(N_particle_batches):
+    for j in range(N_internal):
+        for k in range(N_perturbers):
+            with open(outputfile,"a") as f:
+                f.write( "{:d} {:d} {:d} \n".format(NPs[i],j,k) )
+
+print("can run ",N_batches," simulations at once")
+# loop over perturber index 
